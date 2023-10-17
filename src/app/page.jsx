23730a6d7e3893e308b389635e74670d617431e9
BeatrizSanti <rm551856@fiap.com.br>
2023-10-17 09:55:26 -0300

    Fazendo Produto

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,16 @@
-import Image from 'next/image'
+import Link from "next/link";
+
 
 export default function Home() {
   return (
     <>
       <h1>HOME PAGE</h1>
 
+      <div>
+        <Link href = "/produtos/tenis"> Tênis </Link> |
+        <Link href = "/produtos/produto/tenis-preto"> Tênis Preto </Link> |
+        <Link href = "/produtos/produto/camisa-xadrez"> Camisa </Link>
+      </div>
     </>
   )
 }
