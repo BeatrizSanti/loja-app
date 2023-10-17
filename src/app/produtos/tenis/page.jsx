75033f8d217d11e42a0f2dc5773b055a541bd62e
BeatrizSanti <rm551856@fiap.com.br>
2023-10-17 09:54:36 -0300

    Fazendo produtos

---
+++ src/app/produtos/tenis/page.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+
+export default function tenis() {
+  return (
+    <>
+        <h1>Tenis</h1>
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic inventore expedita facere quis officiis minus vel nulla nisi possimus aliquid ex, odio modi eos est recusandae? Quam accusantium ducimus ad.</p>
+            <p><Link href="/">Voltar... </Link></p>
+        </div>    
+    </>
+  )
+}
