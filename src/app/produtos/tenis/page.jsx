77dc3afcd90ed8a839176fd18cf8b36524ebe0f6
BeatrizSanti <rm551856@fiap.com.br>
2023-10-17 09:57:42 -0300

    Add voltar da page tenis

--- src/app/produtos/tenis/page.jsx
+++ src/app/produtos/tenis/page.jsx
@@ -7,7 +7,7 @@ export default function tenis() {
         <h1>Tenis</h1>
         <div>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic inventore expedita facere quis officiis minus vel nulla nisi possimus aliquid ex, odio modi eos est recusandae? Quam accusantium ducimus ad.</p>
-            <p><Link href="/">Voltar... </Link></p>
+            <p><Link href="/">Voltar... </Link></p>  
         </div>    
     </>
   )
