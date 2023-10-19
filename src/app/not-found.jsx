050defb59ffcbb14985721bf53c6d86bf0765bed
BeatrizSanti <rm551856@fiap.com.br>
2023-10-19 19:57:53 -0300

    Add cabeçalho e rodapé

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -2,7 +2,7 @@ export default function Notfound() {
     return (
      
         <div>
-           <h1>HOME PAGE</h1>  
+           <h1>OOPS! 404</h1>  
             <p>Sorry, the page you are looking for does not exist!</p>
         </div>
     )
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,11 +6,6 @@ export default function Home() {
     <>
       <h1>HOME PAGE</h1>
 
-      <div>
-        <Link href = "/produtos/tenis"> Tênis </Link> |
-        <Link href = "/produtos/produto/tenis-preto"> Tênis Preto </Link> |
-        <Link href = "/produtos/produto/camisa-xadrez"> Camisa </Link>
-      </div>
     </>
   )
 }
