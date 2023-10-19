ae48384fc5a6a89c63daf2ea3ca1ecfe9fa73bb4
BeatrizSanti <rm551856@fiap.com.br>
2023-10-19 08:35:36 -0300

    Page not-found add

---
+++ src/app/not-found.jsx
@@ -0,0 +1,10 @@
+export default function Notfound() {
+    return (
+     
+        <div>
+           <h1>HOME PAGE</h1>  
+            <p>Sorry, the page you are looking for does not exist!</p>
+        </div>
+    )
+  }
+  
