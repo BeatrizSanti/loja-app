b11dcafe490d965de11a5a73b8ff124c794debcf
BeatrizSanti <rm551856@fiap.com.br>
2023-11-07 09:25:20 -0300

    Arrumando rodape

--- src/components/Rodape/rodape.jsx
+++ src/components/Rodape/rodape.jsx
@@ -4,7 +4,7 @@ export default function Rodape() {
   return (
     <footer className="bg-teal-900 text-white h-16 fixed w-full bottom-0">
       <div>
-        <p>Desenvolvido por: <span>Ana, Mirelly e Beatriz</span></p>
+        <p>Desenvolvido por: <span>Beatriz Santi</span></p>
         <p>NEXT.js - React.js - GitHub - tailwindcss</p>
       </div>
     </footer>
