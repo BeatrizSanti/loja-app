a5bcd44002493210040841bb240f9e19158c89e5
BeatrizSanti <rm551856@fiap.com.br>
2023-11-07 10:00:30 -0300

    Arrumando Imagens

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,20 +6,6 @@ export default function Home() {
     <div>
       <br/>
       <h1>home</h1>
-        <br/>
-        <p>CP3 - Ex2</p>
-          <br/>
-
-          <p>1 - Criar uma API de usuários utilizando o gerador de dados Data-Generator.<br/></p>
-          <p>2 - Estilizar usando o SASS/tailwind/css-module. <br/> </p>
-          <p>3 - Utilize funções assíncronas.<br/></p> 
-          <p>4 - Crie os métodos GET-ALL e GET-BY-ID<br/></p>
-          <p>5 - Crie a view destes usuários.<br/></p> 
-          <p>6 - A view do GET-ALL-USERS deve apresentar todos os usuários em uma tabela, com  todos os seus respectivos dados, mais duas opções que devem ser identificadas através de ícones e links sobre os ícones.(Dica, utilize react-icons para os icons, ou similar).<br/></p> 
-          <p>7 - A view do GET-BY-ID, deve ter um link sobre o nome do usuário que deve remeter o usuário para uma página que vai apresentar apenas aquele usuário.<br/></p> 
-          <p>8 - Tudo feito dentro do nosso projeto loja-app.<br/></p> 
-          <p>9 - Padronização.<br/></p> 
-          <p>10 - Todo o procedimento das views deve ser realizado dentro de uma única pasta.</p> 
     </div>
   )
 }
